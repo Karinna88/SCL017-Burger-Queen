843ea4916c2e3bc5fbed9ac9dc1f38c85f1b2209
Karinna <karinna.sepulveda.matamala@gmail.com>
2021-10-10 12:52:56 -0300

    se realiza función de eliminar y se mejora funcion de agregar productos

--- src/Componentes/Menu.js
+++ src/Componentes/Menu.js
@@ -2,17 +2,16 @@ import React, { useEffect } from "react";
 import db from "../firebase/firebaseConfig";
 import NavBar from "./NavBar";
 import "../../src/css/Menu.css";
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
 
 
 const Menu = () => {
-
- const {menu} = useParams();
- console.log(menu)
-
-  const [pedidos, setPedidos] = React.useState([]);
+  const { menu } = useParams();
+  const [productList, setProductList] = React.useState([]);
   const [menus, setMenus] = React.useState([]);
 
+
+  // se carga el menú
   useEffect(() => {
     db.collection("menu")
       .where("categoria", "==", menu)
@@ -23,52 +22,64 @@ const Menu = () => {
           })
         );
       });
-
   }, []);
 
-
+  // funcion para agregar nuevo producto
   const agregar = (item) => {
-    console.log(item)
- 
-      let pedido = {
-        nombre: item.item,
-        cantidad: 1,
-        total: item.precio*1
-      }
+    console.log(item);
+    const productListClone = [...productList]; //esto representa a todos los productos (nuevoproducto = clon)
 
-    setPedidos([
-      ...pedidos, pedido
-    ])
+    productListClone.push({ //agregar un producto 
+      id: item.id,
+      nombre: item.item,
+      precio: item.precio,
+      cantidad: 1,
+      total: item.precio * 1,
+    });
+
+      setProductList(productListClone)
+
   };
 
-  
+
+  const eliminar = (id) => {
+    let arrayFilter = productList.filter(item => item.id != id )
+    setProductList(arrayFilter)
+  }
+
+
+
+
+  // Elemento:  Card y button
   return (
     <div>
       <div className="container">
-       <NavBar/>
+        <NavBar />
 
         <div className="row">
           <div className="col-6">
             <div>
-                {
-                  menus.map((item) => (
-                    <button
-                      className="menuButton"
-                      key={item.id}
-                      onClick={(e) => agregar(item)}
-                    >
-                      <img src={item.url} alt="icono-producto" className="iconButton" width={50}></img>
-                      <div className="product-text-button">
-                        <p>{item.item}</p> <p>${item.precio}</p>
-                      </div>
-                    </button>
-                  ))
-                }
-              </div>
-           
+              {menus.map((item) => (
+                <button
+                  className="menuButton"
+                  key={item.id}
+                  onClick={(evento) => agregar(item)}
+                >
+                  <img
+                    src={item.url}
+                    alt="icono-producto"
+                    className="iconButton"
+                    width={50}
+                  ></img>
+                  <div className="product-text-button">
+                    <p>{item.item}</p> <p>${item.precio}</p>
+                  </div>
+                </button>
+              ))}
+            </div>
           </div>
 
-          {/* tabla pedido */}
+          {/* Elemento:  tabla pedido */}
 
           <div className="col-6">
             <table className="table">
@@ -81,18 +92,16 @@ const Menu = () => {
                 </tr>
               </thead>
               <tbody>
-                {
-                   pedidos.map((pedido, key) => (
+                {productList.map((pedido, key) => (
                   <tr key={key}>
                     <th scope="row">{pedido.nombre}</th>
                     <td>{pedido.cantidad}</td>
                     <td>{pedido.total}</td>
                     <td>
-                      <button className="btn btn-danger">Eliminar</button>
+                      <button onClick={(evento) => eliminar(pedido.id)} className="btn btn-danger">Eliminar</button>
                     </td>
                   </tr>
-                  ))
-                }
+                ))}
               </tbody>
             </table>
           </div>
